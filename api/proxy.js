fc79a57d0c40c05f0ab719208ce5c476a2415ee2
Hex <67011212055@msu.ac.th>
2026-05-01 23:57:51 +0700

    Update proxy.js

--- api/proxy.js
+++ api/proxy.js
@@ -35,7 +35,6 @@ module.exports = async (req, res) => {
       'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36',
       'Accept': 'application/json, text/plain, */*',
       'Accept-Language': 'en-US,en;q=0.9',
-      'Accept-Encoding': 'gzip, deflate, br',
       'Referer': 'https://wichxshop.com/',
       'Connection': 'keep-alive',
       'Content-Type': 'application/json'
